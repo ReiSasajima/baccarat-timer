f59b49c358d94eab89a39ecb8f12037fc186c2c7
Rei Sasajima <sasajimarei@gmail.com>
2022-10-30 01:33:56 +0900

    add timer function

--- timer.py
+++ timer.py
@@ -1,1 +1,20 @@
-import time
+import schedule
+from time import sleep
+import datetime
+import pytz
+
+#01 定期実行する関数を準備
+def task():
+    # 今の時間を取得
+    now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+    print(now)
+#02 スケジュール登録
+# 毎時間指定分に実行
+# schedule.every().hour.at(":33").do(task)
+# 数秒ごとに実装
+schedule.every(10).seconds.do(task)
+
+#03 イベント実行
+while True:
+    schedule.run_pending()
+    sleep(1)
