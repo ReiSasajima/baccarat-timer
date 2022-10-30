aa509a6cbf49c5203655fe415f268dc43e6b426d
Rei Sasajima <sasajimarei@gmail.com>
2022-10-30 18:20:15 +0900

    fixed for final stage

--- timer.py
+++ timer.py
@@ -9,18 +9,20 @@ def forty():
     # 今の時間を取得
     now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
     print(f'今の時間は{now}です！10分前です!')
-    playsound("「やっほー」.mp3")
+    playsound("警告音1.mp3")
+    playsound("警告音1.mp3")
 
 def fifty():
     now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
     print(f'今は{now}です！カジノしていただきありがとうございました!')
-    playsound("「さようなら」.mp3")
+    playsound("成功音.mp3")
+    playsound("成功音.mp3")
 
 
 # 40分になったらfortyの実行
-schedule.every().hour.at(":07").do(forty)
+schedule.every().hour.at(":40").do(forty)
 # 50分になったらfiftyの実行
-schedule.every().hour.at(":08").do(fifty)
+schedule.every().hour.at(":50").do(fifty)
 # 数秒ごとに実装
 # schedule.every(10).seconds.do(forty)
 
